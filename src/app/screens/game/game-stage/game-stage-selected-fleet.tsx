c95ad8cdc191c842b8fccd9987c267a1bfcf9164
tschaul <julian.steinwachs@fau.de>
2020-03-03 16:15:57 +0100

    fix warp order path glitch

--- src/app/screens/game/game-stage/game-stage-selected-fleet.tsx
+++ src/app/screens/game/game-stage/game-stage-selected-fleet.tsx
@@ -64,7 +64,7 @@ export class GameStageSelectedFleet extends React.Component<{
 
       result.push(
         <line
-          key={destinationWorldId}
+          key={result.length}
           x1={arrowStart.x}
           y1={arrowStart.y}
           x2={arrowEnd.x}
@@ -95,6 +95,7 @@ export class GameStageSelectedFleet extends React.Component<{
       const arrowEnd = add(arrowStart, delta);
 
       return <line
+        key="current"
         x1={arrowStart.x}
         y1={arrowStart.y}
         x2={arrowEnd.x}
