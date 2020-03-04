4c5b31f715cb36eb77d50dd85d1567acb41a5f8d
tschaul <julian.steinwachs@fau.de>
2020-03-04 10:51:59 +0100

    test for visibility

---
+++ src/core/tests/visibility/visibility-test-map.ts
@@ -0,0 +1,68 @@
+import { GameState } from "../../../shared/model/v1/state";
+
+export const visibilityTestMap: GameState = {
+  currentTimestamp: 0,
+  gameEndTimestamp: Number.MAX_VALUE,
+  universe: {
+    visibility: {
+      p1: {},
+      p2: {}
+    },
+    fleets: {
+      "f1": {
+        id: "f1",
+        status: 'READY',
+        combatStatus: 'AT_PEACE',
+        currentWorldId: "w1",
+        metal: 0,
+        orders: [],
+        ownerId: "p1",
+        ships: 10,
+        integrity: 1
+      }
+    },
+    worlds: {
+      "w1": {
+        status: 'READY',
+        orders: [],
+        id: "w1",
+        industry: 5,
+        metal: 40,
+        mines: 1,
+        ownerId: "p1",
+        population: 25,
+        ships: 5,
+        integrity: 1,
+        combatStatus: 'AT_PEACE',
+        miningStatus: 'NOT_MINING'
+      },
+      "w2": {
+        status: 'LOST',
+        orders: [],
+        id: "w2",
+        industry: 5,
+        metal: 40,
+        mines: 1,
+        population: 25,
+        ships: 0,
+        integrity: 1,
+      },
+      "w3": {
+        status: 'LOST',
+        orders: [],
+        id: "w3",
+        industry: 5,
+        metal: 40,
+        mines: 1,
+        population: 25,
+        ships: 0,
+        integrity: 1,
+      }
+    },
+    gates: {
+      "w1": ["w2"],
+      "w2": ["w1","w3"],
+      "w3": ["w2"]
+    }
+  }
+}
---
+++ src/core/tests/visibility/visibility.test.ts
@@ -0,0 +1,35 @@
+import { visibilityTestMap } from "./visibility-test-map";
+
+import { expect } from 'chai';
+import { ReadyFleet } from "../../../shared/model/v1/fleet";
+import produce from "immer";
+import { runMap } from "../test-helper";
+
+describe("visibility", () => {
+
+  it("warps back and forth remebers world", async () => {
+    
+    const map = produce(visibilityTestMap, draft => {
+
+      draft.universe.fleets["f1"].orders.push({
+        type: 'WARP',
+        targetWorldId: "w2"
+      })
+  
+      draft.universe.fleets["f1"].orders.push({
+        type: 'WARP',
+        targetWorldId: "w1"
+      })
+
+    });
+    
+    const state = await runMap(map);
+
+    expect((state.universe.visibility['p1']['w1']).status).to.equal("VISIBLE")
+    expect((state.universe.visibility['p1']['w2']).status).to.equal("VISIBLE")
+    expect((state.universe.visibility['p1']['w3']).status).to.equal("REMEBERED")
+  })
+})
+
+
+
